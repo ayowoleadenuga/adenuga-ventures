e85bac1ba5b0136b9bf385e4271a90190b1c48bd
Ayowole <ayowoleadenuga@gmail.com>
2019-08-25 20:08:42 +0100

    Updates

---
+++ precache-manifest.5b73cc8fd8ec3c1fd1ccbdba087df49d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f0fd17b1a197991dbe426c24789218",
+    "url": "/ayowoleadenuga/adenuga-ventures/index.html"
+  },
+  {
+    "revision": "ef4eddc7f01870a3b29a",
+    "url": "/ayowoleadenuga/adenuga-ventures/static/css/main.4084fb9c.chunk.css"
+  },
+  {
+    "revision": "fcce35d815684ee6bfa1",
+    "url": "/ayowoleadenuga/adenuga-ventures/static/js/2.6cb64258.chunk.js"
+  },
+  {
+    "revision": "ef4eddc7f01870a3b29a",
+    "url": "/ayowoleadenuga/adenuga-ventures/static/js/main.d34e66ee.chunk.js"
+  },
+  {
+    "revision": "bb5a6767c5b0f30eee3f",
+    "url": "/ayowoleadenuga/adenuga-ventures/static/js/runtime~main.9d936e62.js"
+  },
+  {
+    "revision": "d66bf615feaa428f6e25fa9808fc8076",
+    "url": "/ayowoleadenuga/adenuga-ventures/static/media/crown.d66bf615.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ayowoleadenuga/adenuga-ventures/static/media/shopping-bag.30f07071.svg"
+  }
+]);
